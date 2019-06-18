7e3bba02ae86eaf9799d887f4ff4b181c407225f
marcell <juhaszmarcell46@gmail.com>
2019-06-18 17:27:20 +0200

    serializer: TransportPseudoHeader: deserializer part FIXME: not a great condition in if statement

--- src/inet/transportlayer/common/TransportPseudoHeaderSerializer.cc
+++ src/inet/transportlayer/common/TransportPseudoHeaderSerializer.cc
@@ -52,7 +52,40 @@ void TransportPseudoHeaderSerializer::serialize(MemoryOutputStream& stream, cons
 
 const Ptr<Chunk> TransportPseudoHeaderSerializer::deserialize(MemoryInputStream& stream) const
 {
+
+#if CHUNK_CHECK_IMPLEMENTATION_ENABLED
+
+    auto transportPseudoHeader = makeShared<TransportPseudoHeader>();
+    b startPosition = b(stream.getPosition());
+    std::cout << startPosition << endl;
+    std::vector<uint8_t> bytes;
+    for(int i = 0; i < 12; ++i){
+        bytes.push_back(stream.readByte());
+    }
+    if(bytes[8] == 0){
+        transportPseudoHeader->setNetworkProtocolId(Protocol::ipv4.getId());
+        transportPseudoHeader->setSrcAddress(L3Address(Ipv4Address(bytes[0], bytes[1], bytes[2], bytes[3])));
+        transportPseudoHeader->setDestAddress(L3Address(Ipv4Address(bytes[4], bytes[5], bytes[6], bytes[7])));
+        // bytes[8]
+        transportPseudoHeader->setProtocolId(bytes[9]);
+        transportPseudoHeader->setPacketLength(B(((uint16_t)bytes[10] << 8) + (uint16_t)bytes[11]));
+    }
+    else {
+        stream.seek(startPosition);
+        transportPseudoHeader->setNetworkProtocolId(Protocol::ipv6.getId());
+        transportPseudoHeader->setSrcAddress(L3Address(stream.readIpv6Address()));
+        transportPseudoHeader->setDestAddress(L3Address(stream.readIpv6Address()));
+        transportPseudoHeader->setPacketLength(B(stream.readUint32Be()));
+        stream.readByte();
+        stream.readByte();
+        stream.readByte();
+        transportPseudoHeader->setProtocolId(stream.readByte());
+    }
+    return transportPseudoHeader;
+#elif
     throw cRuntimeError("TransportPseudoHeader is not a valid deserializable data");
+#endif
+
 }
 
 } // namespace inet
