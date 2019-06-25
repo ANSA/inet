e5f005d79b126c86397fc6a9bd1e229269373bb1
marcell <juhaszmarcell46@gmail.com>
2019-06-25 12:01:02 +0200

    serializer: Ieee8021qHeaderSerializer: fix

--- src/inet/linklayer/ieee8021q/Ieee8021qHeaderSerializer.cc
+++ src/inet/linklayer/ieee8021q/Ieee8021qHeaderSerializer.cc
@@ -25,34 +25,39 @@ void Ieee8021qHeaderSerializer::serialize(MemoryOutputStream& stream, const Ptr<
 {
     const auto& ieee8021qHeader = staticPtrCast<const Ieee8021qHeader>(chunk);
     stream.writeUint16Be(0x8100);
-    stream.writeBit(ieee8021qHeader->getPcp() & 4 == 4);
-    stream.writeBit(ieee8021qHeader->getPcp() & 2 == 2);
-    stream.writeBit(ieee8021qHeader->getPcp() & 1 == 1);
+    stream.writeNBitsOfUint64Be(ieee8021qHeader->getPcp(), 3);
+    //stream.writeBit(ieee8021qHeader->getPcp() & 4 == 4);
+    //stream.writeBit(ieee8021qHeader->getPcp() & 2 == 2);
+    //stream.writeBit(ieee8021qHeader->getPcp() & 1 == 1);
     stream.writeBit(ieee8021qHeader->getDe());
-    uint16_t m = 2048; // 2^11
+    stream.writeNBitsOfUint64Be(ieee8021qHeader->getVid(), 12);
+    /*uint16_t m = 2048; // 2^11
     for(uint8_t i = 0; i < 12; ++i){
         stream.writeBit(ieee8021qHeader->getVid() & m == m);
         m /= 2;
-    }
+    }*/
 }
 
 const Ptr<Chunk> Ieee8021qHeaderSerializer::deserialize(MemoryInputStream& stream) const
 {
     auto ieee8021qHeader = makeShared<Ieee8021qHeader>();
     stream.readUint16Be();
-    uint8_t pcp = 0;
+    ieee8021qHeader->setPcp(stream.readNBitsToUint64Be(3));
+    /*uint8_t pcp = 0;
     pcp += stream.readBit() ? 4 : 0;
     pcp += stream.readBit() ? 2 : 0;
-    pcp += stream.readBit() ? 1 : 0;
-    ieee8021qHeader->setPcp(pcp);
+    pcp += stream.readBit() ? 1 : 0;*/
+    //ieee8021qHeader->setPcp(pcp);
     ieee8021qHeader->setDe(stream.readBit());
-    uint16_t m = 2048; // 2^11
+    ieee8021qHeader->setVid(stream.readNBitsToUint64Be(12));
+    /*uint16_t m = 2048; // 2^11
     uint16_t vid = 0;
     for(uint8_t i = 0; i < 12; ++i){
         vid += stream.readBit() ? m : 0;
         m /= 2;
     }
-    ieee8021qHeader->setVid(vid);
+    ieee8021qHeader->setVid(vid);*/
+    return ieee8021qHeader;
 }
 
 } // namespace inet
